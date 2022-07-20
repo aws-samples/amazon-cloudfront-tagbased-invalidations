c9364cc3d9046be62a72d09a16fbfa632560d0d7
Jaiganesh Girinathan <ganeshji@amazon.com>
2022-07-20 12:20:20 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-## My Project
+## Tag based Invalidations - Amazon CloudFront
 
-TODO: Fill this README out!
+This is a reference implementation on how you could implement 'Tag-based' invalidations in CloudFront.
 
-Be sure to:
+## Pre-requisites:
 
-* Change the title in this README
-* Edit your repository description on GitHub
+1. Install 'make'.
+1. Install git.
+
+
+## Steps to build
+
+1. Clone this repository using command `git clone https://github.com/aws-samples/amazon-cloudfront-tagbased-invalidations.git`
+
+2. In the 'Makefile'
+- set 'bucket' variable to the Amazon S3 bucket which will hold your build artifacts.
+- set 'region' variable to the AWS Region where you want the deployed solution to reside.
+- set 'profile' variable to the AWS CLI profile which has necessary permissions to deploy AWS resources using CloudFormation.
+- set 'backend_endpoint' variable to the backend system which will act as 'Origin' for CloudFront. This is the backend which is emitting the tag metadata in response headers.
+
+3. Run `make all`. This would build the project and copy the assets into an S3 bucket and deploy the CloudFormation template in the AWS region set..
 
 ## Security
 
@@ -14,4 +27,3 @@ See [CONTRIBUTING](CONTRIBUTING.md#security-issue-notifications) for more inform
 ## License
 
 This library is licensed under the MIT-0 License. See the LICENSE file.
-
---
+++ lambda-functions/origin-response/index.js
@@ -0,0 +1,40 @@
+var AWS = require("aws-sdk");
+var util = require("util");
+var parentRegionSNS = new AWS.SNS({ region: '${ParentRegion}'});
+var localRegionSNS = new AWS.SNS();
+var tagName = "${TagFieldName}";
+var topicName = "${TopicName}";
+var accountId = "${AccountId}";
+var parentRegion = "${ParentRegion}";
+var region = process.env.AWS_REGION;
+var topicArn = util.format("arn:aws:sns:%s:%s:%s",region,accountId,topicName);
+
+exports.handler = async (event) => {
+    console.log("Received event:%j", event);
+    let response = event.Records[0].cf.response
+    if(response.headers[tagName]){
+      let request = event.Records[0].cf.request;
+      let tagPayload = {};
+      tagPayload.distributionId = event.Records[0].cf.config.distributionId;
+      tagPayload.querystring = request.querystring;
+      tagPayload.uri = request.uri;
+      tagPayload.tags = response.headers[tagName][0].value;
+      // delete the tag header before sending back the response
+      delete response.headers[tagName];
+      console.log("Tag payload :%j",tagPayload);
+      var params = {
+          Message: JSON.stringify(tagPayload),
+          Subject: "PostTag"+tagPayload.distributionId,
+          TopicArn: topicArn
+      };
+      try{
+        await localRegionSNS.publish(params).promise();
+      }
+      catch(e){
+          console.log("Trying in parent region:%s",e);
+          params.TopicArn = util.format("arn:aws:sns:%s:%s:%s",parentRegion,accountId,topicName);
+          await parentRegionSNS.publish(params).promise();
+      }
+    }
+    return response;
+};
