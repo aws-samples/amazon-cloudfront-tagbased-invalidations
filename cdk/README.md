41c0ec8908ef405c2cdad732eadcd2504b65054f
Jaiganesh Girinathan <ganeshji@amazon.com>
2022-11-27 12:56:50 -0500

    updated README

--- cdk/README.md
+++ cdk/README.md
@@ -21,4 +21,23 @@ This is a reference implementation on how you could implement tag-based invalida
 
 1. Clone the repository and change into `cdk` directory.
 1. Set the environment variables in env.sh
+```
+# Required: set the AWS CLI profile name
+export AWS_PROFILE="default"
+# Optional: set the SNS topic name
+export TOPIC_NAME='tag-ingest-topic'
+# Required: set the AWS region where the primary deployment of solution resides.
+export PRIMARY_AWS_REGION='us-west-1'
+# Required: Specify delimiter when multiple tags are passed, possible values ',' or ' ' (space)
+export TAG_DELIMITER=','
+# Required: Specify the response header name where tags are present
+export TAG_NAME='Edge-Cache-Tag'
+# Optional: Specify how to determine the TTL of the Tag reocord in DynamoDB.
+# whether to use Cache-Control header max-age or take the TTL specified as part of the tag
+# leave it empty if you do not want to set a TTL 
+# possible values 'Tag' or 'Cache-Control' or ''
+export TAG_TTL_DEFINED_BY='Cache-Control'
+# deploy a sample application that consists of CloudFront + Lambda Function as origin
+export SAMPLE_APP_SETUP='True'
+```
 1. Run ./deploy.sh
--- cdk/env.sh
+++ cdk/env.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+# Required: set the AWS CLI profile name
 export AWS_PROFILE="default"
+# Optional: set the SNS topic name
 export TOPIC_NAME='tag-ingest-topic'
+# Required: set the AWS region where the primary deployment of solution resides.
 export PRIMARY_AWS_REGION='us-west-1'
-# Specify delimiter when multiple tags are passed
+# Required: Specify delimiter when multiple tags are passed, possible values ',' or ' ' (space)
 export TAG_DELIMITER=','
-# Specify how to determine the TTL of the reocord in DynamoDB.
-# whether to use Cache-Control header max-age or take the TTL specified as part of the tag 
-# possible values 'Tag' | 'Cache-Control'
+# Required: Specify the response header name where tags are present
 export TAG_NAME='Edge-Cache-Tag'
+# Optional: Specify how to determine the TTL of the Tag reocord in DynamoDB.
+# whether to use Cache-Control header max-age or take the TTL specified as part of the tag
+# leave it empty if you do not want to set a TTL 
+# possible values 'Tag' or 'Cache-Control' or ''
 export TAG_TTL_DEFINED_BY='Cache-Control'
+# deploy a sample application that consists of CloudFront + Lambda Function as origin
 export SAMPLE_APP_SETUP='True'
