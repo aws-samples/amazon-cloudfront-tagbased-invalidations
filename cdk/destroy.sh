474653afcbcceba92fb2e2fe42d450e54214a71a
Jaiganesh Girinathan <ganeshji@amazon.com>
2023-02-15 11:24:41 -0500

    clean up codes

--- cdk/destroy.sh
+++ cdk/destroy.sh
@@ -10,7 +10,7 @@ REC_DEPLOY_AWS_REGIONS=('us-east-1' 'us-east-2' 'us-west-2' 'ap-south-1'
 'ap-northeast-1' 'ap-northeast-2' 'ap-southeast-1' 'ap-southeast-2' 'eu-west-1' 
 'eu-west-2' 'eu-central-1' 'sa-east-1')
 
-REC_DEPLOY_AWS_REGIONS=('us-east-1' 'us-east-2')
+# REC_DEPLOY_AWS_REGIONS=('us-east-1' 'us-east-2')
 
 # Deploy the stack in the primary AWS Region
 export CDK_DEPLOY_REGION=$PRIMARY_AWS_REGION
